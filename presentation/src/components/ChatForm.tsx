4bc02d61ee1698266f0fd963dcf95fc98f9e1a2d
Olasunkanmi Oyinlola <olasunkanmi@regovtech.com>
2024-03-25 16:34:59 +0800

    addthe skeleton loader

--- presentation/src/components/ChatForm.tsx
+++ presentation/src/components/ChatForm.tsx
@@ -12,20 +12,6 @@ import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import DOMPurify from "dompurify";
 import { formatText } from "../utils";
 
-// interface Message {
-//   text: string;
-//   metaData?: {
-//     documentId: number;
-//     pageNumber: number;
-//   };
-// }
-
-// interface Iresponse {
-//   history: { role: string; parts: { text: string }[] }[];
-//   question: string;
-//   answer: string;
-// }
-
 interface IHistory {
   role: string;
   parts: { text: string }[];
@@ -36,6 +22,7 @@ export function Thread() {
   const [error, setError] = useState("");
   const [question, setQuestion] = useState("");
   const [chatHistory, setChatHistory] = useState<IHistory[]>([]);
+  const [loading, setLoading] = useState(false);
 
   const formAction = async () => {
     if (!question) {
@@ -43,12 +30,13 @@ export function Thread() {
       return;
     }
     try {
+      setLoading(true)
+      setQuestion("");
       const response = await axiosPrivate.post("/chat", {
         question,
         chatHistory: JSON.stringify(chatHistory),
       });
       const data = response.data;
-      console.log(JSON.stringify(data.data.chatHistory));
       setChatHistory((oldChat) => [
         {
           role: "user",
@@ -60,12 +48,13 @@ export function Thread() {
         },
         ...oldChat,
       ]);
-      setQuestion("");
+      setLoading(false)
       return data;
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
       console.error(error);
       setError(error.message);
+      setLoading(false)
     }
   };
 
@@ -96,11 +85,11 @@ export function Thread() {
                   value={question}
                   onChange={(e) => setQuestion(e.target.value)}
                 />
-                <Button variant="outline-success" onClick={formAction}>
+                <Button variant="outline-success" onClick={formAction} disabled={loading}>
                   Send
                 </Button>
                 <div className="vr" />
-                <Button variant="outline-danger" onClick={clearChat}>
+                <Button variant="outline-danger" onClick={clearChat} disabled={loading}>
                   Reset
                 </Button>
               </Stack>
@@ -110,7 +99,12 @@ export function Thread() {
           <div>
             <p>{error}</p>
           </div>
-          {chatHistory.map((chatItem, index) => (
+          <div>
+          {loading ? (
+            <><div className="loading-skeleton" style={{ marginBottom: "10px", marginTop: "10px", height: "70px" }}></div><div className="loading-skeleton" style={{ marginBottom: "10px", marginTop: "10px", height: "140px" }}></div></>
+          ) : ""}
+          </div>
+          <div>{chatHistory.map((chatItem, index) => (
             <Card
               style={{ marginBottom: "10px", marginTop: "10px" }}
               key={index}
@@ -126,7 +120,8 @@ export function Thread() {
                 </Card.Body>
               ))}
             </Card>
-          ))}
+          ))}</div>
+          
         </Col>
         <Col></Col>
       </Row>
